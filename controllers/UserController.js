28932c61c1014349bab03f16121ac4262404d075
glen-s-abraham <glenprojects666@gmail.com>
2021-10-16 10:53:37 +0530

    feat:update other user data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('./../models/User');
 const catchAsync = require('./../utils/CatchAsync');
+const AppError = require('./../utils/AppError');
 
 exports.getAllUsers = catchAsync(async (req,res,next)=>{
     const users = await User.find();
@@ -39,3 +40,30 @@ exports.deleteUser = (req,res)=>{
         message:'route not implemented'
     });
 }
+
+const filterObj = (obj, ...allowedFields)=>{
+    const newObj = {};
+    Object.keys(obj).forEach(el=>{
+        if(allowedFields.includes(el)){
+            newObj[el]=obj[el];
+        }
+    });
+    return newObj;
+}
+
+exports.updateMe = catchAsync(async (req,res,next)=>{
+    if(req.body.password || req.body.passwordConfirm){
+        return next(new AppError('This route is not for password updates',400));
+    }
+    const filteredBody = filterObj(req.body,'name','email');
+    user = await User.findByIdAndUpdate(
+        req.user.id,
+        filteredBody,
+        {new:true,
+        runValidators:true}
+    );
+    res.status(200).json({
+        status:'success',
+        data:{user}
+    });
+});
