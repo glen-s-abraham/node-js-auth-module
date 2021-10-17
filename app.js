eb12b4345acf5177a5a058142e406b4cbe4ac8ec
glen-s-abraham <glenprojects666@gmail.com>
2021-10-17 11:41:57 +0530

    refactor:added security packages

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const morgan = require('morgan');
 const app = express();
 const AppError = require('./utils/AppError');
@@ -8,7 +13,25 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+//Header security
+app.use(helmet());
+//Rate limiter
+const limiter = rateLimit({
+    max:100,
+    windowMs:60*60*1000,
+    message:'Too many requests from this IP Please try again in an hour'
+});
+app.use('/api', limiter);
+
 app.use(express.json());
+
+//Data sanitization NoSql injection
+app.use(mongoSanitize());
+//Data Sanitization XSS
+app.use(xss());
+//parameter pollution
+app.use(hpp());
+
 app.use('/api/users',userRouter);
 //error handler for unhandled routes
 app.all('*',(req,res,next)=>{
